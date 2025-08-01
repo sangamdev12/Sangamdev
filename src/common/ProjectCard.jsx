66ab603448b023924bf4247c9a782044a8751ecb
sangamdev12 <rijalsangam129@gmail.com>
2025-08-01 00:26:02 -0400

    finished project

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+// src/common/ProjectCard.jsx
+import React from 'react';
 import styles from '../sections/Projects/ProjectsStyles.module.css';
 
-function ProjectCard({ src, link, h3, p}) {
+function ProjectCard({ src, h3, p, liveLink, codeLink }) {
   return (
-    
-      <a href={link} target='_blank'>
-        <div className={styles.cardimg}>
-       <img id='cardImg' className='hover' src={src} alt={`${h3} logo`}/></div>
+    <div className={styles.card}>
+      <div className={styles.cardImgContainer}>
+        <img src={src} alt={h3} className={styles.cardImg} />
+      </div>
+      <div className={styles.cardContent}>
         <h3>{h3}</h3>
-      <p>{p}</p>
-      </a>
-  )
+        <div className={styles.hoverInfo}>
+          <p>{p}</p>
+        </div>
+      </div>
+      <div className={styles.cardFooter}>
+        <a href={codeLink} target="_blank" rel="noopener noreferrer">
+          <button className={styles.codeBtn}>Code</button>
+        </a>
+        {liveLink && (
+          <a href={liveLink} target="_blank" rel="noopener noreferrer" style={{ marginLeft: '10px' }}>
+            <button className={styles.codeBtn}>Live</button>
+          </a>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
+// src/sections/Projects.jsx
+import React from 'react';
 import styles from './ProjectsStyles.module.css';
 import viberr from '../../assets/viberr.png';
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
-    <section id='projects' className={styles.container}>
-        <h1 className='sectionTitle'>Projects</h1>
-        <div className={styles.projectsContainer}>
-           <ProjectCard src={viberr} link='https://github.com/sangamdev12'
-           h3 = "viberr"
-           description = "Streaming app"
-           />
-
-           <ProjectCard src={viberr} link='https://github.com/sangamdev12/My-ToDo-Application'
-           h3 = "ToDo App"
-           p = "Task manager"
-           />
-
-           <ProjectCard src={viberr} link='https://github.com/sangamdev12'
-           h3 = "SPR"
-           p = "gaming app"
-           /><br /> <br />
-        </div>
-
-
+    <section id="projects" className={styles.projectsSection}>
+      <h1 className={styles.heading}>Projects</h1>
+      <div className={styles.projectsGrid}>
+        <ProjectCard
+          src={viberr}
+          h3 = "Viberr"
+          p="Streaming app for music & video."
+          liveLink="https://example.com/viberr"
+          codeLink="https://github.com/sangamdev12/viberr"
+        />
+        <ProjectCard
+          src={viberr}
+          h3="Todo App"
+          p="A simple to-do list built with React."
+          liveLink="https://mygolist.netlify.app/"
+          codeLink="https://github.com/sangamdev12/My-ToDo-Application"
+        />
+        <ProjectCard
+          src={viberr}
+          h3="SPR"
+          p="A gaming app built with JS and HTML5 Canvas."
+          liveLink="https://example.com/spr"
+          codeLink="https://github.com/sangamdev12/spr"
+        />
+      </div>
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
